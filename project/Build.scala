641dae738207887c79be17b704c2cd80ef26d86f
pocketberserker <pocketberserker@gmail.com>
2015-08-27 04:13:15 +0900

    dog 0.1.1

--- project/Build.scala
+++ project/Build.scala
@@ -24,7 +24,7 @@ object build extends Build {
     ),
     ScriptedPlugin.scriptedLaunchOpts ++= Seq(
       "-Dplugin.version=" + version.value,
-      "-Ddog.version=0.1.0"
+      "-Ddog.version=0.1.1"
     ),
     startYear := Some(2015),
     organization := "com.github.pocketberserker",
